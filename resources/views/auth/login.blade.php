3894502c45c02ecf2b59b5ff23bdfef918d45935
Yogarozan <siyogarozan123@gmail.com>
2025-11-27 16:53:09 +0700

    feat: designing login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,19 @@
 <x-auth-layout>
     <x-slot:title>Login</x-slot:title>
-    <h1>Login Page wkwkw</h1>
+    <main class="min-h-screen flex flex-col justify-center items-center">
+
+        <form action="">
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <legend class="fieldset-legend">Login</legend>
+
+                <label class="label">Email</label>
+                <input type="email" class="input" placeholder="Email" />
+
+                <label class="label">Password</label>
+                <input type="password" class="input" placeholder="Password" />
+
+                <button class="btn btn-neutral mt-4">Login</button>
+            </fieldset>
+        </form>
+    </main>
 </x-auth-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,25 @@
 <x-auth-layout>
     <x-slot:title>Register</x-slot:title>
-    <h1>Register Page</h1>
+<main class="min-h-screen flex flex-col justify-center items-center">
+
+        <form action="">
+            <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+                <legend class="fieldset-legend">Register</legend>
+                <label class="label">Nama</label>
+                <input type="nama" class="input" placeholder="Nama" />
+
+                <label class="label">Email</label>
+                <input type="email" class="input" placeholder="Email" />
+
+                <label class="label">Password</label>
+                <input type="password" class="input" placeholder="Password" />
+
+
+                <label class="label">NO. HP</label>
+                <input type="hp" class="input" placeholder="NO. HP" />
+
+                <button class="btn btn-neutral mt-4">Register</button>
+            </fieldset>
+        </form>
+    </main>
 </x-auth-layout>
